5127e6c2c94c1e4e82326a6fb72e840e6e0f9ef8
paulb <paulbentsen@hotmail.com>
2021-02-10 13:30:02 +0100

    begynnelse på robot class

---
+++ src/main/java/inf112/skeleton/app/Robot.java
@@ -0,0 +1,43 @@
+package inf112.skeleton.app;
+
+public class Robot {
+    public int initialHealth=100;
+    public boolean initialDeathStatus=false;
+
+    public int initialCheckpoint=0;
+
+    public int posX=0;
+    public int posY=0;
+
+    //trenger en variabel som forteller hvilken retning roboten står (0,1,2,3?)
+
+    /*public void keyPressed(KeyEvent e)
+    {
+
+        if (e.getKeyCode()==39)
+        {
+            //Right arrow key code
+            //her skal roboten rotere 90 grader mot høyre
+        }
+
+        else if (e.getKeyCode()==37)
+        {
+            //Left arrow key code
+            //her skal roboten rotere 90 grader mot venstre
+        }
+
+        else if (e.getKeyCode()==38)
+        {
+            //Up arrow key code
+            //her skal roboten bevege seg fremover den retningen den står
+        }
+
+        else if (e.getKeyCode()==40)
+        {
+            //down arrow key code
+            //her skal roboten bevege seg bakover i forhold til retning
+        }
+
+    }*/
+
+}
