985c390678dc404f111e40264d067671df299fdf
hfl022 <herman.fleischer@student.uib.no>
2021-02-18 15:14:42 +0100

    Assets contains a Tiledmap New Class, TileMap

---
+++ src/main/java/inf112/skeleton/app/TileMap.java
@@ -0,0 +1,55 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+
+public class TileMap extends ApplicationAdapter{
+    Texture img;
+    TiledMap tiledMap;
+    OrthographicCamera camera;
+    TiledMapRenderer tiledMapRenderer;
+    SpriteBatch sb;
+    Texture texture;
+    Sprite sprite;
+
+    @Override
+    public void create() {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
+        camera.update();
+
+        tiledMap = new TmxMapLoader().load(String.valueOf(Gdx.files.internal("src/assets/Tilemap/untitled.tmx")));
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        sb = new SpriteBatch();
+        //texture = new Texture(Gdx.files.internal("pik.png"));
+        //sprite = new Sprite(texture);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+        sb.begin();
+        sb.end();
+    }
+}
